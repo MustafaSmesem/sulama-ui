99cf2204943c7f258d8d28945b233f7cc63ee063
mustafas <mustafa.samism@comodo.com>
2023-03-06 20:37:15 +0300

    commit

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ t('hello') }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelloI18n',
+  setup() {
+
+    // Something todo ..
+
+  }
+})
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import 'flowbite'
 import './assets/css/tailwind.css'
-import './assets/css/style.css'
+// import './assets/css/style.css'
+// import i18n from './i18n'
 
 createApp(App).use(store).use(router).mount('#app')
